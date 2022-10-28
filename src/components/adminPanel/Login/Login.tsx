68db4ac56c751fbf3785c6f6c57787551dde7bb0
Artemy <artemij87work@gmail.com>
2022-10-28 15:14:57 +0400

    fix: clean Login component and shared/Buttons

--- src/components/adminPanel/Login/Login.tsx
+++ src/components/adminPanel/Login/Login.tsx
@@ -1,52 +1,10 @@
-import React, {FC} from "react";
-import {Helmet} from "react-helmet";
+import React from 'react';
 
-import {Link} from "react-router-dom";
-import ButtonPrimary from "../../../shared/Button/ButtonPrimary";
-import Input from "../../../shared/Input/Input";
-
-export interface PageLoginProps {
-    className?: string;
-}
-
-const PageLogin: FC<PageLoginProps> = ({className = ""}) => {
+export const Login = () => {
     return (
-        <div className={`nc-PageLogin ${className}`} data-nc-id="PageLogin">
-            <Helmet>
-                <title>Login || Booking React Template</title>
-            </Helmet>
-            <div className="container mb-24 lg:mb-32">
-                <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
-                    Login
-                </h2>
-                <div className="max-w-md mx-auto space-y-6">
-                    {/* FORM */}
-                    <form className="grid grid-cols-1 gap-6" action="#" method="post">
-                        <label className="block">
-                            <span className="text-neutral-800 dark:text-neutral-200">
-                                Email address
-                            </span>
-                            <Input
-                                type="email"
-                                placeholder="example@example.com"
-                                className="mt-1"
-                            />
-                        </label>
-                        <label className="block">
-                            <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">
-                              Password
-                              <Link to="/forgot-pass" className="text-sm">
-                                Forgot password?
-                              </Link>
-                            </span>
-                            <Input type="password" className="mt-1"/>
-                        </label>
-                        <ButtonPrimary type="submit">Continue</ButtonPrimary>
-                    </form>
-                </div>
-            </div>
+        <div>
+            Login
         </div>
     );
 };
 
-export default PageLogin;
