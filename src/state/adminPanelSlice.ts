807a5974505d16467a2aa9f7cd51d5168223223d
Anton Metlitsky <ticket1201@gmail.com>
2022-11-24 17:06:04 +0400

    feat: impl fetchDataTC and extra reducer for complete thunk

--- src/state/adminPanelSlice.ts
+++ src/state/adminPanelSlice.ts
@@ -1,42 +1,62 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {ApplicantsDataType} from "../models/applicantModel";
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {ApplicantsDataType} from '../models/applicantModel';
+import {firebaseAPI} from '../firebase/firebase';
+import {setStatus} from './appSlice';
+
+
+export const fetchDataTC = createAsyncThunk('adminPanel/fetchData', async (param, {
+    dispatch,
+    rejectWithValue
+}) => {
+    dispatch(setStatus({status: 'loading'}))
+    try {
+        let res: InitialStateType = await firebaseAPI.getData()
+        return res
+    } catch {
+        return rejectWithValue(null)
+    } finally {
+        dispatch(setStatus({status: 'idle'}))
+    }
+})
 
 export const adminPanelSlice = createSlice({
     name: 'adminPanel',
     initialState: [{
+        appDate: '',
         applicantsData: [],
-        email: "",
+        email: '',
         file: [],
         fullPrice: 0,
-        numberOfApplicants: "",
-        service: "",
-        tel: "",
-        uid: "",
-        visa_status: "",
-        visitPurpose: ""
+        numberOfApplicants: '',
+        service: '',
+        tel: '',
+        uid: '',
+        visa_status: '',
+        visitPurpose: ''
     }] as InitialStateType,
-    reducers: {
-        setApplicants: (state, action) => {
-            state.push(action.payload);
-        },
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(fetchDataTC.fulfilled, (state, action) => {
+            return action.payload
+        })
     }
 })
 
 export default adminPanelSlice.reducer;
-export const {setApplicants} = adminPanelSlice.actions;
 
 //types
-export type InitialStateType = [
-    {
-        applicantsData: ApplicantsDataType[]
-        email: string
-        file: []
-        fullPrice: number
-        numberOfApplicants: string
-        service: string
-        tel: string
-        uid: string
-        visa_status: string
-        visitPurpose: string
-    }
-];
+export type InitialStateType = Array<ApplicationType>;
+
+type ApplicationType = {
+    appDate: string
+    applicantsData: ApplicantsDataType[]
+    email: string
+    file: []
+    fullPrice: number
+    numberOfApplicants: string
+    service: string
+    tel: string
+    uid: string
+    visa_status: string
+    visitPurpose: string
+}
