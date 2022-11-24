999cf571431117f5f82404302225e426725297aa
Anton Metlitsky <ticket1201@gmail.com>
2022-11-24 17:11:02 +0400

    feat: rework loginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,30 +1,16 @@
 import React from 'react'
-import {Navigate} from 'react-router-dom'
-
 import Grid from '@mui/material/Grid';
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import {useAuth} from 'src/hooks/use-auth';
-import {useAppSelector} from "src/hooks/hooks";
-import {ErrorSnackbar} from "src/common/ErrorSnackBar/ErrorSnackBar";
 import {LoginForm} from "../../components/LoginForm";
 
 export const LoginPage = () => {
-    console.log('LoginPage')
-
-    const {isAuth} = useAuth();
-    const error = useAppSelector(state => state.appSlice.error);
-
-    if (isAuth) {
-        return <Navigate to='/panel'/>
-    }
     return (
         <Grid container justifyContent={'center'}>
-            {error && <ErrorSnackbar/>}
             <Grid item justifyContent={'center'}>
-                <Toolbar style={{alignItems: 'center', margin: '70px 0 10px 0'}}>
-                    <Typography variant="h3" gutterBottom>
-                        Login
+                <Toolbar style={{margin: '70px 0 10px 0', justifyContent:'center'}}>
+                    <Typography variant="h4" gutterBottom>
+                        Sign in
                     </Typography>
                 </Toolbar>
                 <LoginForm />
