4b6f51a52bc72dfe486f67a367be40cf7e8b85cf
Anton Metlitsky <ticket1201@gmail.com>
2022-11-25 20:35:47 +0400

    feat: rework date min width

--- src/components/adminPanel/table-admin-panel/TableForAdminPanel.tsx
+++ src/components/adminPanel/table-admin-panel/TableForAdminPanel.tsx
@@ -3,12 +3,40 @@ import Box from '@mui/material/Box';
 import {DataGridPro, DataGridProProps} from '@mui/x-data-grid-pro';
 import {Row} from '../AdminPanel';
 import DetailedTable from './DetailedTable';
+import IconButton from '@mui/material/IconButton';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import {useState} from 'react';
 
 type Props = { rows: Row[] }
+/*const commonModalState = {
+    _id: '',
+    cardsPack_id: '',
+    name: '',
+    private: false as boolean | undefined,
+    question: '',
+    answer: '',
+    title: '',
+    packCover: '',
+    questionURL: '',
+    answerURL: '',
+    openAddPackModal: false as boolean | undefined,
+    openEditPackModal: false as boolean | undefined,
+    openDelPackModal: false as boolean | undefined,
+    openAddCardModal: false as boolean | undefined,
+    openEditCardModal: false as boolean | undefined,
+    openDelCardModal: false
+}
+type CommonModalStateType = typeof commonModalState*/
 
-export default function TableForAdminPanel(props: Props) {
+
+export default function TableForAdminPanel({rows}: Props) {
+   /* const [modalData, setModalData] = useState<CommonModalStateType>(commonModalState)
+    const openDeletePackModal = (_id: string, name: string) => {
+        setModalData({...modalData, _id, name, title: 'Delete Pack', openDelPackModal: true})
+    }*/
+
     const columns = [
-        {field: 'appDate', headerName: 'Date', minWidth: 200},
+        {field: 'appDate', headerName: 'Date', flex: 1, minWidth: 140},
         {field: 'email', headerName: 'Email', flex: 1, minWidth: 220},
         {field: 'file', headerName: 'Files', flex: 1},
         {field: 'fullPrice', headerName: 'Service Price', flex: 1},
@@ -18,9 +46,17 @@ export default function TableForAdminPanel(props: Props) {
         {field: 'visaStatus', headerName: 'Visa Status', flex: 1},
         {field: 'visitPurpose', headerName: 'Visit purpose', flex: 1},
         {field: 'uid', flex: 1},
+    /* {filed: 'actions',  headerName: 'Actions', flex:1,
+         renderCell: (params) => (
+             <>
+                     <IconButton onClick={() => openDeletePackModal(params.row._id, params.row.name)}>
+                         <DeleteOutlineIcon/>
+                     </IconButton>
+             </>
+         ),
+        }*/
     ];
 
-    const {rows} = props
     const getDetailPanelHeight = React.useCallback(() => 400, []);
 
     const getDetailPanelContent = React.useCallback<NonNullable<DataGridProProps['getDetailPanelContent']>>(({row}) =>
