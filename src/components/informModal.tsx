a8cc20e5dd0a96406ba8a5cfc05da3a393edd215
Anton Metlitsky <ticket1201@gmail.com>
2022-11-26 16:10:01 +0400

    feat: impl InformModal component

---
+++ src/components/informModal.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+type PropsType = {
+    isOpen: boolean
+    title: string
+    description: string
+    actionName: string
+    onClose: () => void
+    onSuccess: () => void
+}
+
+const InformModal = ({title, description, onClose, onSuccess, isOpen, actionName}:PropsType) => {
+
+    const handleClose = () => {
+        onClose();
+    };
+
+    return (
+        <Dialog onClose={handleClose} open={isOpen}>
+            <div className={'m-7 flex flex-col'}>
+                <DialogTitle variant={'h5'}>{title}</DialogTitle>
+                <Typography>{description}</Typography>
+                <div className={'flex justify-between mt-8'}>
+                    <Button onClick={onClose} variant={'outlined'}>Close</Button>
+                    <Button onClick={onSuccess} variant={'contained'}>{actionName}</Button>
+                </div>
+            </div>
+        </Dialog>
+    );
+};
+
+export default InformModal;
